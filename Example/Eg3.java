68de36d76945e49de969832526bd58dc95096cc1
ntn-Huy <huynguyen310106@gmail.com>
2026-08-11 20:03:33 +0700

    add file eg3

---
+++ Example/Eg3.java
@@ -0,0 +1,18 @@
+public class Eg3 {
+    public static void main (String[] args) {
+        String name1 = "Java";
+        String name2 = new String("Java");
+        // so sanh dia chi o nho sai
+        System.out.println("Dung == : " + (name1==name2)); // false
+       // so sanh dung
+        System.out.println("Dung equal: "+ name1.equals(name2)); // true
+        
+        // cac cach dung cua string
+        String text = "  Lap trinh Java  ";
+        System.out.println("Text goc: "+text);
+        System.out.println("Do dai: "+text.length());
+        System.out.println("IN HOA: "+text.toUpperCase());
+        System.out.println("Xoa khoang trang: "+text.trim());
+    }
+    
+}
