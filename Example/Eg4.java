1a310f01acc6a6da32361012c6de1f55c51cda60
ntn-Huy <huynguyen310106@gmail.com>
2026-08-12 12:54:49 +0700

    test

--- Example/Eg4.java
+++ Example/Eg4.java
@@ -1,6 +1,19 @@
-public class Eg4 {
+/*
+    Nhap vao 1 so nguyen n va in ra so dao nguoc cua no
+*/
 
-    public static void main (String[] args) {
-        System.out.println("ok");
+import java.util.Scanner;
+public class Eg4 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao 1 so nguyen n: ");
+        int n = scanner.nextInt();
+        int reverse = 0;
+        while(n != 0){
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n /= 10;
+        }
+        System.out.println("So dao nguoc cua no la: " + reverse);
     }
 }
