d6ce709df88be19a0bccc388410b40bb867ad97c
ntn-Huy <huynguyen310106@gmail.com>
2026-08-12 10:25:41 +0700

    add file Eg5:tinh diem

--- Example/Eg3.java
+++ Example/Eg3.java
@@ -3,7 +3,7 @@ public class Eg3 {
         String name1 = "Java";
         String name2 = new String("Java");
         // so sanh dia chi o nho sai
-        System.out.println("Dung == : " + (name1==name2)); // false
+        System.out.println("Dung == : " + (name1 == name2)); // false
        // so sanh dung
         System.out.println("Dung equal: "+ name1.equals(name2)); // true
         
@@ -13,6 +13,11 @@ public class Eg3 {
         System.out.println("Do dai: "+text.length());
         System.out.println("IN HOA: "+text.toUpperCase());
         System.out.println("Xoa khoang trang: "+text.trim());
+
+        // Noi chuoi StringBuilder
+        StringBuilder sb = new StringBuilder();
+        sb.append("xin chao: ").append("Lap trinh vien ").append("Java");
+        System.out.println(sb.toString());
     }
     
 }
---
+++ Example/Eg5.java
@@ -0,0 +1,72 @@
+/* Bai toan tinh diem vote
+    * Diem thuong ky: Neu 10 vote vao cac cot thuong ky (co 3 cot)
+    * Neu 20 vote cong 2 diem vao tung cot cua diem thuong ky
+    * Neu 50 vote cong 1 diem vao tung cot cua diem Gk && 3 cot thuong ky moi cot 2 diem
+    * Neu 100 vote cong 2 diem vao cot cua diem GK && 3 cot thuong ky moi cot 3 diem
+    * Output:
+    * Nhap sinh vien
+    *  Nhap vote
+    * Tu dong in ra cot diem thuong ky va diem giua ky
+*/
+
+
+import java.util.Scanner;
+public class Eg5 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        // Nhap so ten
+        System.out.print("Nhap ten sv: ");
+        String name = scanner.nextLine();
+
+        // Nhap diem
+        System.out.print("Nhap diem tk 1: ");
+        double tk1 = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Nhap diem tk 2: ");
+        double tk2 = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Nhap diem tk 3: ");
+        double tk3 = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.print("Nhap diem gk: ");
+        double gk = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Nhap so vote: ");
+        int vote = scanner.nextInt();
+
+        //logic cong diem
+        int diemCongTk = 0;
+        int diemCongGk =0 ;
+        if (vote >=100){
+            diemCongTk =3;
+            diemCongGk =2;
+        } else if (vote >= 50){
+            diemCongTk =2;
+            diemCongGk=2;
+        } else if (vote >=20){
+            diemCongTk =2;
+        } else if (vote >= 10){
+            diemCongTk=1;
+        }
+        // Cong diem
+        tk1 += diemCongTk;
+        tk2 += diemCongTk;
+        tk3 += diemCongTk;
+        gk += diemCongGk;
+        if (tk1>10){tk1=10;}
+        if (tk2>10){tk2=10;}
+        if (tk3>10){tk3=10;}
+        if (gk>10){gk=10;}
+        // Hien thi
+        System.out.println("Sinh vien: "+name);
+        System.out.println("Diem tk 1: "+tk1);
+        System.out.println("Diem tk 2: "+tk2);
+        System.out.println("Diem tk 3: "+tk3);
+        System.out.println("Diem gk : "+gk);
+        scanner.close();
+    }
+    
+
+    
+}
